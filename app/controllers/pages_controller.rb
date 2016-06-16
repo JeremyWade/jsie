82cf90c7c94fa85bca8f10d45e42c34cc7973cd2
Jeremy Wade <jeremy.wade.10@gmail.com>
2016-06-16 14:45:51 -0500

    generated pages controller with home and research pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
   end
+  def research
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/home'
+  root 'pages#home'
+  get "research" => "pages#research"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
