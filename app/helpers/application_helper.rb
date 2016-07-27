83a0166b6b8e54434f54bd1fb0517961960e9a06
Jeremy Wade <jeremy.wade.10@gmail.com>
2016-07-27 17:07:08 +0530

    updated video in learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+def embed(youtube_url)
+    youtube_id = youtube_url.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+ end
 end
