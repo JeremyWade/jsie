4d60a904d18294184d08f1c8a15bd07dca6c9d41
Jeremy Wade <jeremy.wade.10@gmail.com>
2016-10-26 16:10:17 +0530

    fixed broken seq link

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   get "startup" => "pages#startup"
   get "lec" => "pages#lec"
   get "blog" => "pages#blog"
-  get '/research/:id', to: 'research#show'
-  get '/research/:id', to: 'research#show', as: 'seq'
+  get "research/seq" => "pages#seq"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
