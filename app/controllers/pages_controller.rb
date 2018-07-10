b15ba2e17418ad59acedae4578bb103e2123d520
JeremyWade <jeremy.wade.10@gmail.com>
2018-07-10 18:15:35 +0530

    added new page for ppc program

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,4 +25,6 @@ class PagesController < ApplicationController
   end
   def bitimpact
   end
+  def ppc
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get "about/mukulkumar" => "pages#mukulkumar"
   get "startupacademy" => "pages#startupacademy"
   get "bitimpact" => "pages#bitimpact"
+  get "ppc" => "pages#ppc"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
