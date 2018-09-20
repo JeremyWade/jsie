55c0bc5147950d48aede48c17058a97ab2c76a41
JeremyWade <jeremy.wade.10@gmail.com>
2018-09-20 13:35:11 +0530

    added impactislamabad page and updated about page

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get "startupacademy" => "pages#startupacademy"
   get "bitimpact" => "pages#bitimpact"
   get "ppc" => "pages#ppc"
+  get "impactislamabad" => "pages#impactislamabad"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
